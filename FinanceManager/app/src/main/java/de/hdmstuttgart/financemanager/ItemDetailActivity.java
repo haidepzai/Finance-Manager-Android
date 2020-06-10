998aadebc22ba6090d9c697597f812b8d3664a59
Leon Heinrich <l_heinrich@outlook.com>
2020-06-10 18:07:36 +0200

    Re-structured Layout for ItemDetailActivity and re-designed the Edit-Process

--- FinanceManager/app/src/main/java/de/hdmstuttgart/financemanager/ItemDetailActivity.java
+++ FinanceManager/app/src/main/java/de/hdmstuttgart/financemanager/ItemDetailActivity.java
@@ -3,13 +3,11 @@ package de.hdmstuttgart.financemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,21 +22,16 @@ import de.hdmstuttgart.financemanager.Fragments.MainFragment;
 
 public class ItemDetailActivity extends AppCompatActivity {
 
-    private FloatingActionButton fab;
-    private Dialog myDialog; //Popup Fenster
+    private FloatingActionButton fab_done;
 
     private DatePickerDialog.OnDateSetListener mDateSetListener;
-    private Button editBill;
+
     //TextView welches in der Activity angezeigt wird
-    private TextView mPurpose;
-    private TextView mAmount;
+    private EditText mPurpose;
+    private EditText mAmount;
     private TextView mDate;
-    private TextView mMethod;
-    //EditText in dem Popup Dialog
-    private EditText payPurpose;
-    private EditText payAmount;
-    private TextView mDisplayDate;
-    private EditText payMethod;
+    private EditText mMethod;
+
     //position des aktuellen Eintrages in der Liste
     private int position;
 
@@ -47,6 +40,7 @@ public class ItemDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail);
 
+
         Intent in = this.getIntent();
         String purpose = in.getStringExtra("Purpose");
         String amount = in.getStringExtra("Amount");
@@ -54,16 +48,13 @@ public class ItemDetailActivity extends AppCompatActivity {
         String method = in.getStringExtra("Method");
         position = in.getIntExtra("Position", 0);
 
-        myDialog = new Dialog(this);
-
         //Change Date
         mDateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                mDisplayDate = myDialog.findViewById(R.id.inputDate);
                 month = month + 1;
                 String date = dayOfMonth + "." + month + "." + year;
-                mDisplayDate.setText(date);
+                mDate.setText(date);
             }
         };
 
@@ -77,35 +68,21 @@ public class ItemDetailActivity extends AppCompatActivity {
         mDate.setText(date);
         mMethod.setText(method);
 
-        fab = findViewById(R.id.fab_edit);
-        fab.setOnClickListener(new View.OnClickListener() {
+        deactivateText();
+
+        fab_done = findViewById(R.id.fab_done);
+        fab_done.hide();
+        FloatingActionButton fab_edit = findViewById(R.id.fab_edit);
+        fab_edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView txtClose;
-                myDialog.setContentView(R.layout.custompopup);
 
-                txtClose = myDialog.findViewById(R.id.txtClose);
-                txtClose.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        myDialog.dismiss();
-                    }
-                });
-                myDialog.show();
-                //Initialisierung der TextViews in dem Dialog
-                payPurpose = myDialog.findViewById(R.id.inputPurpose);
-                payAmount = myDialog.findViewById(R.id.inputAmount);
-                payMethod = myDialog.findViewById(R.id.inputMethod);
-                mDisplayDate = myDialog.findViewById(R.id.inputDate);
-
-                //Setzt den Anfangstext der Edit Felder im Dialog
-                payPurpose.setText(mPurpose.getText().toString());
-                payAmount.setText(mAmount.getText().toString());
-                payMethod.setText(mMethod.getText().toString());
-                mDisplayDate.setText(mDate.getText().toString());
-
-                //Öffnet Datum-Feld
-                mDisplayDate.setOnClickListener(new View.OnClickListener() {
+                mPurpose.setFocusableInTouchMode(true);
+                mPurpose.setFocusable(true);
+                mAmount.setFocusableInTouchMode(true);
+                mAmount.setFocusable(true);
+                mDate.setClickable(true);
+                mDate.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Calendar cal = Calendar.getInstance();
@@ -121,31 +98,51 @@ public class ItemDetailActivity extends AppCompatActivity {
                         dialog.show();
                     }
                 });
-                //Button im Dialog um Rechnung hinzuzufügen
-                editBill = myDialog.findViewById(R.id.addBill);
-                editBill.setText("Bestätigen");
-                editBill.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //Verändert den Inhalt der Liste
-                        TransactionItem.itemList.set(position, new TransactionItem(R.drawable.ic_euro_black,
-                                payPurpose.getText().toString(),
-                                payAmount.getText().toString(),
-                                mDisplayDate.getText().toString(),
-                                payMethod.getText().toString()
-                        ));
-                        //Ändert den Inhalt in der ItemDetailActivity
-                        mPurpose.setText(payPurpose.getText().toString());
-                        mAmount.setText(payAmount.getText().toString());
-                        mDisplayDate.setText(mDisplayDate.getText().toString());
-                        mMethod.setText(payMethod.getText().toString());
+                mMethod.setFocusableInTouchMode(true);
+                mMethod.setFocusable(true);
 
-                        MainFragment.mAdapter.notifyDataSetChanged();
-                        myDialog.dismiss();
-                    }
-                });
+                mPurpose.setBackgroundResource(R.drawable.edit_border);
+                mAmount.setBackgroundResource(R.drawable.edit_border);
+                mDate.setBackgroundResource(R.drawable.edit_border);
+                mMethod.setBackgroundResource(R.drawable.edit_border);
+
+                fab_done.show();
+
+            }
+
+
+        });
+
+        fab_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TransactionItem.itemList.set(position, new TransactionItem(R.drawable.ic_euro_black,
+                        mPurpose.getText().toString(),
+                        mAmount.getText().toString(),
+                        mDate.getText().toString(),
+                        mMethod.getText().toString()
+                ));
+
+                deactivateText();
+                fab_done.hide();
+                MainFragment.mAdapter.notifyDataSetChanged();
             }
         });
 
+    }
+
+    private void deactivateText() {
+
+        mPurpose.setFocusable(false);
+        mAmount.setFocusable(false);
+        mDate.setFocusable(false);
+        mDate.setClickable(false);
+        mMethod.setFocusable(false);
+
+        mPurpose.setBackground(null);
+        mAmount.setBackground(null);
+        mDate.setBackground(null);
+        mMethod.setBackground(null);
     }
 }
