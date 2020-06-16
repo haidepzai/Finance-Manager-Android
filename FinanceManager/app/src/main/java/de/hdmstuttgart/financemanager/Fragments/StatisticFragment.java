62f97b9e2f41dc8cfb3172f188bb3cc4efddb988
Son Hai Vu <sv048@hdm-stuttgart.de>
2020-06-16 22:21:43 +0200

    Implemented Statistic

--- FinanceManager/app/src/main/java/de/hdmstuttgart/financemanager/Fragments/StatisticFragment.java
+++ FinanceManager/app/src/main/java/de/hdmstuttgart/financemanager/Fragments/StatisticFragment.java
@@ -1,6 +1,7 @@
 package de.hdmstuttgart.financemanager.Fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import java.text.DecimalFormat;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import de.hdmstuttgart.financemanager.R;
 import de.hdmstuttgart.financemanager.Database.Transaction;
@@ -19,8 +22,19 @@ import de.hdmstuttgart.financemanager.View.BarChartView;
 
 public class StatisticFragment extends Fragment {
 
-    private double totalAmount;
+    public static float totalAmount;
+
     private BarChartView mBarChartView;
+    private TextView totalAmountView;
+    private TextView yAxisMax;
+    private TextView yAxisHalf;
+
+    public static float totalGrocery;
+    public static float totalFood;
+    public static float totalEducation;
+    public static float totalLeisure;
+    public static float totalFee;
+    public static float totalMisc;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +51,8 @@ public class StatisticFragment extends Fragment {
                 }
             }
         }).start();
+
+
     }
 
 
@@ -46,18 +62,120 @@ public class StatisticFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_statistic, container, false);
 
         mBarChartView = rootView.findViewById(R.id.barChartView);
-        TextView totalAmountView = rootView.findViewById(R.id.totalAmount);
 
+        //Gesamt
+        totalAmountView = rootView.findViewById(R.id.totalAmount);
+        yAxisMax = rootView.findViewById(R.id.yAxisMax);
+        yAxisHalf = rootView.findViewById(R.id.yAxisHalf);
+
+
+        calcTotalAmount();
+        calcGrocery();
+        calcFood();
+        calcEducation();
+        calcLeisure();
+        calcFee();
+        calcMisc();
+
+        return rootView;
+    }
+    private void calcTotalAmount(){
+        totalAmount = 0;
         for(Transaction item : Transaction.outcomingBills){
             String valueOfAmount = item.getmAmount().replaceAll("[-€,]", "");
             totalAmount += Double.parseDouble(valueOfAmount);
         }
         if(totalAmount != 0){
+            yAxisMax.setVisibility(View.VISIBLE);
+            yAxisHalf.setVisibility(View.VISIBLE);
             totalAmountView.setText(new DecimalFormat("#,###.00").format(totalAmount));
+            yAxisMax.setText(totalAmountView.getText().toString());
+            yAxisHalf.setText(new DecimalFormat("#,###.00").format(totalAmount/2));
         } else {
             totalAmountView.setText("0.00 €");
+            yAxisMax.setText("0.00 €");
+            yAxisMax.setVisibility(View.INVISIBLE);
+            yAxisHalf.setVisibility(View.INVISIBLE);
         }
+        Log.d("Total", String.valueOf(totalAmount));
+    }
 
-        return rootView;
+    private void calcGrocery(){
+        totalGrocery = 0;
+        List<Transaction> list = Transaction.outcomingBills
+                .stream()
+                .filter(s -> s.getmCategory().equals("Einkauf"))
+                .collect(Collectors.toList());
+
+        for(Transaction item : list){
+            String valueOfAmount = item.getmAmount().replaceAll("[-€,]", "");
+            totalGrocery += Double.parseDouble(valueOfAmount);
+        }
+        Log.d("Grocery", String.valueOf(totalGrocery));
+    }
+    private void calcFood(){
+        totalFood = 0;
+        List<Transaction> list = Transaction.outcomingBills
+                .stream()
+                .filter(s -> s.getmCategory().equals("Essen"))
+                .collect(Collectors.toList());
+
+        for(Transaction item : list){
+            String valueOfAmount = item.getmAmount().replaceAll("[-€,]", "");
+            totalFood += Double.parseDouble(valueOfAmount);
+        }
+        Log.d("Food", String.valueOf(totalFood));
+    }
+    private void calcEducation(){
+        totalEducation = 0;
+        List<Transaction> list = Transaction.outcomingBills
+                .stream()
+                .filter(s -> s.getmCategory().equals("Studium/Beruf"))
+                .collect(Collectors.toList());
+
+        for(Transaction item : list){
+            String valueOfAmount = item.getmAmount().replaceAll("[-€,]", "");
+            totalEducation += Double.parseDouble(valueOfAmount);
+        }
+        Log.d("Education", String.valueOf(totalEducation));
+    }
+    private void calcLeisure(){
+        totalLeisure = 0;
+        List<Transaction> list = Transaction.outcomingBills
+                .stream()
+                .filter(s -> s.getmCategory().equals("Freizeit"))
+                .collect(Collectors.toList());
+
+        for(Transaction item : list){
+            String valueOfAmount = item.getmAmount().replaceAll("[-€,]", "");
+            totalLeisure += Double.parseDouble(valueOfAmount);
+        }
+        Log.d("Leisure", String.valueOf(totalLeisure));
+    }
+    private void calcFee(){
+        totalFee = 0;
+        List<Transaction> list = Transaction.outcomingBills
+                .stream()
+                .filter(s -> s.getmCategory().equals("Gebühren"))
+                .collect(Collectors.toList());
+
+        for(Transaction item : list){
+            String valueOfAmount = item.getmAmount().replaceAll("[-€,]", "");
+            totalFee += Double.parseDouble(valueOfAmount);
+        }
+        Log.d("Fee", String.valueOf(totalFee));
+    }
+    private void calcMisc(){
+        totalMisc = 0;
+        List<Transaction> list = Transaction.outcomingBills
+                .stream()
+                .filter(s -> s.getmCategory().equals("Sonstige"))
+                .collect(Collectors.toList());
+
+        for(Transaction item : list){
+            String valueOfAmount = item.getmAmount().replaceAll("[-€,]", "");
+            totalMisc += Double.parseDouble(valueOfAmount);
+        }
+        Log.d("Misc", String.valueOf(totalMisc));
     }
 }
--- FinanceManager/app/src/main/java/de/hdmstuttgart/financemanager/View/BarChartView.java
+++ FinanceManager/app/src/main/java/de/hdmstuttgart/financemanager/View/BarChartView.java
@@ -4,32 +4,44 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import de.hdmstuttgart.financemanager.Fragments.StatisticFragment;
+
 public class BarChartView extends View {
 
     private Paint gridPaint = new Paint();
+    private Paint linePaint = new Paint();
 
-    private Rect mBar1;
+    public static float maxAmount;
+    public static float grocery;
+    public static float food;
+    public static float education;
+    public static float leisure;
+    public static float fee;
+    public static float misc;
+
+    private RectF mBar1; //Einkauf
     private Paint mPaintBar1;
 
-    private Rect mBar2;
+    private RectF mBar2; //Essen
     private Paint mPaintBar2;
 
-    private Rect mBar3;
+    private RectF mBar3; //Studium/Beruf
     private Paint mPaintBar3;
 
-    private Rect mBar4;
+    private RectF mBar4; //Freizeit
     private Paint mPaintBar4;
 
-    private Rect mBar5;
+    private RectF mBar5; //Gebühren
     private Paint mPaintBar5;
 
-    private Rect mBar6;
+    private RectF mBar6; //Sonstige
     private Paint mPaintBar6;
 
     public BarChartView(Context context) {
@@ -56,83 +68,158 @@ public class BarChartView extends View {
         init(attrs);
     }
 
-    private void init(@Nullable AttributeSet set){
+    private void init(@Nullable AttributeSet set) {
+
+        maxAmount = StatisticFragment.totalAmount;
+        grocery = StatisticFragment.totalGrocery;
+        food = StatisticFragment.totalFood;
+        education = StatisticFragment.totalEducation;
+        leisure = StatisticFragment.totalLeisure;
+        fee = StatisticFragment.totalFee;
+        misc = StatisticFragment.totalMisc;
+        Log.d("maxAmount", String.valueOf(maxAmount));
+        Log.d("grocery", String.valueOf(grocery));
+        Log.d("food", String.valueOf(food));
+        Log.d("education", String.valueOf(education));
+        Log.d("leisure", String.valueOf(leisure));
+        Log.d("fee", String.valueOf(fee));
+        Log.d("misc", String.valueOf(misc));
+
+
         gridPaint.setColor(Color.BLACK);
         gridPaint.setStrokeWidth(5);
 
-        mBar1 = new Rect();
+        linePaint.setColor(Color.DKGRAY);
+
+        mBar1 = new RectF();
         mPaintBar1 = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintBar1.setColor(Color.GREEN);
 
-        mBar2 = new Rect();
+        mBar2 = new RectF();
         mPaintBar2 = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintBar2.setColor(Color.BLUE);
 
-        mBar3 = new Rect();
+        mBar3 = new RectF();
         mPaintBar3 = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintBar3.setColor(Color.RED);
 
-        mBar4 = new Rect();
+        mBar4 = new RectF();
         mPaintBar4 = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintBar4.setColor(Color.CYAN);
 
-        mBar5 = new Rect();
+        mBar5 = new RectF();
         mPaintBar5 = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintBar5.setColor(Color.MAGENTA);
 
-        mBar6 = new Rect();
+        mBar6 = new RectF();
         mPaintBar6 = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintBar6.setColor(Color.YELLOW);
     }
 
 
     @Override
-    protected void onDraw(Canvas canvas){
+    protected void onDraw(Canvas canvas) {
 
+        drawBar(canvas);
+
+        //Balkendiagram (X-Y-Achse)
+        canvas.drawLine(30, 1500, 30, 500, gridPaint); //Vertikal
+        canvas.drawLine(30, 1500, 1000, 1500, gridPaint); //Horizontal
+        //Horizontale Linien
+        canvas.drawLine(30, 1000, 1000, 1000, linePaint); //Horizontal
+        canvas.drawLine(30, 750, 1000, 750, linePaint); //Horizontal
+        canvas.drawLine(30, 500, 1000, 500, linePaint); //Horizontal
+        canvas.drawLine(30, 1250, 1000, 1250, linePaint); //Horizontal
+    }
+
+    private void drawBar(Canvas canvas) {
         mBar1.left = 60; //Abstand links
-        mBar1.top = 1000; //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
+        mBar1.top = 1500 - ((grocery / maxAmount) * 1000); //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
         mBar1.right = 150; //dicke (+90 zu left)
         mBar1.bottom = 1500; //Start
+        Log.d("Groceryyy", String.valueOf(grocery));
+        Log.d("Maxxxxxxxxxx", String.valueOf(maxAmount));
+        Log.d("Mbaaaaaaaar", String.valueOf(mBar1.top));
 
         canvas.drawRect(mBar1, mPaintBar1);
 
         mBar2.left = 180; //Abstand links (+30 zu right von dem vorigen Balken)
-        mBar2.top = 1200; //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
+        mBar2.top = 1500 - ((food / maxAmount) * 1000); //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
         mBar2.right = 270; //dicke (+90 zu left)
         mBar2.bottom = 1500; //Start
 
         canvas.drawRect(mBar2, mPaintBar2);
 
         mBar3.left = 300; //Abstand links (+30 zu right von dem vorigen Balken)
-        mBar3.top = 750; //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
+        mBar3.top = 1500 - ((education / maxAmount) * 1000); //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
         mBar3.right = 390; //dicke (+90 zu left)
         mBar3.bottom = 1500; //Start
 
         canvas.drawRect(mBar3, mPaintBar3);
 
         mBar4.left = 420; //Abstand links (+30 zu right von dem vorigen Balken)
-        mBar4.top = 900; //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
+        mBar4.top = 1500 - ((leisure / maxAmount) * 1000); //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
         mBar4.right = 510; //dicke (+90 zu left)
         mBar4.bottom = 1500; //Start
 
         canvas.drawRect(mBar4, mPaintBar4);
 
         mBar5.left = 540; //Abstand links (+30 zu right von dem vorigen Balken)
-        mBar5.top = 600; //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
+        mBar5.top = 1500 - ((fee / maxAmount) * 1000); //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
         mBar5.right = 630; //dicke (+90 zu left)
         mBar5.bottom = 1500; //Start
 
         canvas.drawRect(mBar5, mPaintBar5);
 
         mBar6.left = 670; //Abstand links (+30 zu right von dem vorigen Balken)
-        mBar6.top = 1400; //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
+        mBar6.top = 1500 - ((misc / maxAmount) * 1000); //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
         mBar6.right = 760; //dicke (+90 zu left)
         mBar6.bottom = 1500; //Start
 
         canvas.drawRect(mBar6, mPaintBar6);
+    }
 
-        //Balkendiagram (X-Y-Achse)
-        canvas.drawLine(30, 1500, 30, 500, gridPaint); //Vertikal
-        canvas.drawLine(30, 1500, 1000, 1500, gridPaint); //Horizontal
+    private void drawBars(Canvas canvas) {
+        mBar1.left = 60; //Abstand links
+        mBar1.top = 1500; //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
+        mBar1.right = 150; //dicke (+90 zu left)
+        mBar1.bottom = 1500; //Start
+
+        canvas.drawRect(mBar1, mPaintBar1);
+
+        mBar2.left = 180; //Abstand links (+30 zu right von dem vorigen Balken)
+        mBar2.top = 1500; //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
+        mBar2.right = 270; //dicke (+90 zu left)
+        mBar2.bottom = 1500; //Start
+
+        canvas.drawRect(mBar2, mPaintBar2);
+
+        mBar3.left = 300; //Abstand links (+30 zu right von dem vorigen Balken)
+        mBar3.top = 1500; //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
+        mBar3.right = 390; //dicke (+90 zu left)
+        mBar3.bottom = 1500; //Start
+
+        canvas.drawRect(mBar3, mPaintBar3);
+
+        mBar4.left = 420; //Abstand links (+30 zu right von dem vorigen Balken)
+        mBar4.top = 1500; //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
+        mBar4.right = 510; //dicke (+90 zu left)
+        mBar4.bottom = 1500; //Start
+
+        canvas.drawRect(mBar4, mPaintBar4);
+
+        mBar5.left = 540; //Abstand links (+30 zu right von dem vorigen Balken)
+        mBar5.top = 1500; //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
+        mBar5.right = 630; //dicke (+90 zu left)
+        mBar5.bottom = 1500; //Start
+
+        canvas.drawRect(mBar5, mPaintBar5);
+
+        mBar6.left = 670; //Abstand links (+30 zu right von dem vorigen Balken)
+        mBar6.top = 1500; //Höhe (500 = 100%; 1500 = 0%; 1000 = 50%)
+        mBar6.right = 760; //dicke (+90 zu left)
+        mBar6.bottom = 1500; //Start
+
+        canvas.drawRect(mBar6, mPaintBar6);
     }
 }
