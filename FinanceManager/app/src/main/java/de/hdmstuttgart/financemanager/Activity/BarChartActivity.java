08b2962e9dd05978f580f179e40e64ffa4348df7
Son Hai Vu <sv048@hdm-stuttgart.de>
2020-06-17 19:12:28 +0200

    Added TextView for BarChartActivity which shows the total amount in each category

--- FinanceManager/app/src/main/java/de/hdmstuttgart/financemanager/Activity/BarChartActivity.java
+++ FinanceManager/app/src/main/java/de/hdmstuttgart/financemanager/Activity/BarChartActivity.java
@@ -20,16 +20,25 @@ public class BarChartActivity extends AppCompatActivity {
     private TextView yAxisMax;
     private TextView yAxisHalf;
 
+    private TextView groceryView;
+    private TextView foodView;
+    private TextView educationView;
+    private TextView leisureView;
+    private TextView feeView;
+    private TextView miscView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_chart);
 
-        mBarChartView = findViewById(R.id.barChartView);
-
-        totalAmountView = findViewById(R.id.totalAmount);
-        yAxisMax = findViewById(R.id.yAxisMax);
-        yAxisHalf = findViewById(R.id.yAxisHalf);
+        initializeView();
+        groceryView.setVisibility(View.GONE);
+        foodView.setVisibility(View.GONE);
+        educationView.setVisibility(View.GONE);
+        leisureView.setVisibility(View.GONE);
+        feeView.setVisibility(View.GONE);
+        miscView.setVisibility(View.GONE);
 
         if(StatisticFragment.totalAmount != 0){
             totalAmountView.setText(new DecimalFormat("#,###.00").format(StatisticFragment.totalAmount));
@@ -43,6 +52,70 @@ public class BarChartActivity extends AppCompatActivity {
             yAxisMax.setText("0.00 €");
             yAxisMax.setVisibility(View.INVISIBLE);
             yAxisHalf.setVisibility(View.INVISIBLE);
+        }
+
+        setTextGrocery();
+        setTextFood();
+        setTextEducation();
+        setTextLeisure();
+        setTextFee();
+        setTextMisc();
+    }
+
+    private void initializeView(){
+        mBarChartView = findViewById(R.id.barChartView);
+
+        groceryView = findViewById(R.id.textViewGrocery);
+        foodView = findViewById(R.id.textViewFood);
+        educationView = findViewById(R.id.textViewEducation);
+        leisureView = findViewById(R.id.textViewLeisure);
+        feeView = findViewById(R.id.textViewFee);
+        miscView = findViewById(R.id.textViewLMisc);
+
+        totalAmountView = findViewById(R.id.totalAmount);
+        yAxisMax = findViewById(R.id.yAxisMax);
+        yAxisHalf = findViewById(R.id.yAxisHalf);
+    }
+    private void setTextGrocery(){
+        if(StatisticFragment.totalGrocery != 0){
+            String amount = "Einkauf: " + StatisticFragment.totalGrocery + " €";
+            groceryView.setText(amount);
+            groceryView.setVisibility(View.VISIBLE);
+        }
+    }
+    private void setTextFood(){
+        if(StatisticFragment.totalFood != 0){
+            String amount = "Essen: " + StatisticFragment.totalFood + " €";
+            foodView.setText(amount);
+            foodView.setVisibility(View.VISIBLE);
+        }
+    }
+    private void setTextEducation(){
+        if(StatisticFragment.totalEducation != 0){
+            String amount = "Studium/Beruf: " + StatisticFragment.totalEducation + " €";
+            educationView.setText(amount);
+            educationView.setVisibility(View.VISIBLE);
+        }
+    }
+    private void setTextLeisure(){
+        if(StatisticFragment.totalLeisure != 0){
+            String amount = "Freizeit: " + StatisticFragment.totalLeisure + " €";
+            leisureView.setText(amount);
+            leisureView.setVisibility(View.VISIBLE);
+        }
+    }
+    private void setTextFee(){
+        if(StatisticFragment.totalFee != 0){
+            String amount = "Gebühren: " + StatisticFragment.totalFee + " €";
+            feeView.setText(amount);
+            feeView.setVisibility(View.VISIBLE);
+        }
+    }
+    private void setTextMisc(){
+        if(StatisticFragment.totalMisc != 0){
+            String amount = "Sonstige: " + StatisticFragment.totalMisc + " €";
+            miscView.setText(amount);
+            miscView.setVisibility(View.VISIBLE);
         }
     }
 }
